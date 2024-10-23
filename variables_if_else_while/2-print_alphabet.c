fb7c1978eb0f9cdeca13cfda88432338715ad6e1
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-23 09:14:54 +0200

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = '1';
+for (ch = '1'; ch <= '9'; ch++)
+putchar(ch);
+return (0);
+}
