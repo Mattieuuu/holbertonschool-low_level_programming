6a2873a92fd65831e630450aa144350587554dee
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-23 13:40:53 +0200

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 1;
+for (ch = 1; ch <= 10; ch++)
+{
+printf(%d, ch);
+}
+printf('\n');
+return (0);
+}
