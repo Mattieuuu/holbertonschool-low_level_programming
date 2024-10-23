71d8fc2bc61ff96152bf888eca1a1a9680bb0a82
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-23 14:06:14 +0200

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+{
+putchar(ch + '0');
+}
+putchar("\n");
+return (0);
+}
