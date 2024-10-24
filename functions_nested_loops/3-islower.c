a0e50cea315c0e596b7de2a72080ae686ae47902
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-24 14:19:14 +0200

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c);
+{
+if (c >= 'a' && c <= 'z')
+return (1)
+else
+return (0);
+}
+
