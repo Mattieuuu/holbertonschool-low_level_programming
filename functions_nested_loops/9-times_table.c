23d3f6d2677c5a7630d133a157f7a69cc2a03299
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-25 14:10:25 +0200

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,9 +7,9 @@
 void times_table(void)
 {
 int a, b, result;
-for (a = 0; a <= 9, a++)
+for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 9, b++)
+for (b = 0; b <= 9; b++)
 {
 result = a * b;
 if (b == 0)
