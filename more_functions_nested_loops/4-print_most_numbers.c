ab17389ba25e25de1f16376b749b92b4e928fb91
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-28 14:08:15 +0100

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 1 to 9 without 2 and 4
+ *
+ * Description: print numbers from 1 to 9 no  2 and 4 followed by a new line
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+int number;
+for (number = '0'; number <= '9'; number++)
+{
+if (number != '2' && number != '4')
+_putchar(digit);
+}
+_putchar('\n');
+}
