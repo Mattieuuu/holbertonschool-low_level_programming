b39aa7f729e2927aef854ff95e217a4b605a14cc
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-28 11:17:52 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: variable first number
+ * @b: variable second number
+ *
+ * Return: Return 0
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
