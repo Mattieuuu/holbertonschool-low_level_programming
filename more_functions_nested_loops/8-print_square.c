26c9b170da92b850b94669634c2e0354b3e805a6
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-29 11:49:56 +0100

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square in terminal
+ * @size: word to check, size of square
+ * Description: Prints square following by a new line
+ * Return: if size is less than 0
+ */
+
+void print_square(int size);
+{
+int a;
+int b;
+if (int size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
