5bfdba96d914e301a9b1ec42f37f05383c924cb9
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-30 10:00:25 +0100

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the pointer n to 98
+ * @n: Character to check
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+int *n = 98;
+}
