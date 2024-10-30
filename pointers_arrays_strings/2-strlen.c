b31def720abd7c8a851a63836c8d9f70befb11c2
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-30 11:25:23 +0100

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer of string
+ * Description: calculate the length of the string
+ * Return: *s
+ */
+
+int _strlen(char *s)
+{
+return (*s);
+}
