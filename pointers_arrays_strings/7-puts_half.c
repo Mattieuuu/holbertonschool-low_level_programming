343015d8b69ff6a46a0776d055e398a80b4901f7
Mattieuuu <@mattieu.mx@gmail.com>
2024-10-31 14:02:24 +0100

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,19 +5,30 @@
  * puts2 - prints a half of a string
  *
  * @str: the character to check
+ * @a: character to check
+ * @b: character to check
  * Return: void
  */
 
 void puts2(char *str)
 {
 int length = 0;
+int a, b;
 while (str[length] != '\0')
 {
-if (length / 2 == 0)
-{
-_putchar(str[length]);
-}
 length++;
+}
+if (length % 2 == 0)
+{
+n = length : 2;
+}
+else
+{
+n = (length - 1) / 2 + 1;
+}
+for (a = b; a < length; a++)
+{
+_putchar(str[a]);
 }
 _putchar('\n');
 }
