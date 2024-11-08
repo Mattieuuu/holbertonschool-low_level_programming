4f17fee85885ec58ff0b42bd683af28bc4f6cced
Mattieuuu <@mattieu.mx@gmail.com>
2024-11-08 14:42:25 +0100

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
