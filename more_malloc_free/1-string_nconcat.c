c2c49c83c727ac15001a5cdd2ffffa5d2b35b116
Mattieuuu <@mattieu.mx@gmail.com>
2024-11-14 17:15:12 +0100

    1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - main function
+ * @s1: first chain of characters
+ * @s2: second chain to concatenate
+ * @n: ubnsigned int n
+ * Return: return or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *result;
+int len1 = 0;
+int len2 = 0;
+int i, j;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[len1] != '\0')
+{
+len1++;
+}
+while (s2[len2] != '\0')
+{
+len2++;
+}
+result = malloc((len1 + n + 1) * sizeof(char));
+
+if (result == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+{
+result[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+result[i + j] = s2[j];
+}
+result[i + j] = '\0';
+return (result);
+}
