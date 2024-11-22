d4a0af2e63df12c7c2df26357060c2271fdcd062
Mattieuuu <@mattieu.mx@gmail.com>
2024-11-22 12:01:11 +0100

    0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * sum_them_all - Calcule la somme de tous les paramètres
+ * @n: Nombre de paramètres
+ * @...: Liste variable de paramètres à additionner
+ *
+ * Return: La somme de tous les paramètres, ou 0 si n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+int sum = 0;
+unsigned int i;
+if (n == 0)
+{
+return (0);
+}
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+va_end(args);
+return(sum);
+}
