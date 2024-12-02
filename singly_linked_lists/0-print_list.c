0424ad703ae5eecaa432512e1815652e5b69c4e5
Mattieuuu <@mattieu.mx@gmail.com>
2024-12-02 13:35:04 +0100

    0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of chain list list_t
+ * @h: pointer for first node of the list
+ *
+ * Return : count (number of node in the list)
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
+}
