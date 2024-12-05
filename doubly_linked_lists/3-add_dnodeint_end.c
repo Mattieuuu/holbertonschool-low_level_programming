755381f62049d28a5fe63dd98bb37785ddce9c00
Mattieuuu <@mattieu.mx@gmail.com>
2024-12-05 11:14:48 +0100

    3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - prints all elements of the list
+ * @head: pointer for head of list
+ * @n: Value to stock in new node
+ *
+ * Return: adress of new node or null if allocation fail
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+dlistint_t *temp;
+
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
+
+if (*head != NULL)
+*head= new_node;
+return (new_node);
+
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+
+temp->next = new_node;
+new_node->prev = temp;
+
+return (new_node);
+}
