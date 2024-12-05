be79d55b26e5ce0491e50ead076b5cd4e16d8724
Mattieuuu <@mattieu.mx@gmail.com>
2024-12-05 13:53:29 +0100

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - prints all elements of the list
+ * @head: pointer for head of list
+ *
+ * Return: adress of new node or null if allocation fail
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *new_node;
+dlistint_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
